72809e3a02d08b13ba113a9ea8ed09c992320e47
DarkHorseDigital <darkhorse.digital@ya.ru>
2026-09-20 09:34:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // При каждом обновлении index.html меняйте эту версию (например, v4, v5 и т.д.)
-const CACHE_NAME = 'tableapp-v3'; 
+const CACHE_NAME = 'tableapp-v4'; 
 
 // Кэшируем только тяжелую статику. index.html сюда НЕ добавляем!
 const ASSETS = [
